3a9aa0884bc53a589a83be779124ab03c6b416f5
insanelyharsh <harshmyadav2002@gmail.com>
2023-10-02 16:08:17 +0530

    created backend file structure

---
+++ Boredom-Slayer-Backend/src/main/java/com/insanelyharsh/BoredomSlayerBackend/BoredomSlayerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.insanelyharsh.BoredomSlayerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoredomSlayerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoredomSlayerBackendApplication.class, args);
+	}
+
+}
---
+++ Boredom-Slayer-Backend/src/test/java/com/insanelyharsh/BoredomSlayerBackend/BoredomSlayerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insanelyharsh.BoredomSlayerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoredomSlayerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
